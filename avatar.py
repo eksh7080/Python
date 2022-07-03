28e747e6ec434a399a6e5f86bea5c10d6adfcf63
eksh7080 <eksh7080@naver.com>
2022-07-03 16:38:30 +0900

    pang pang game

---
+++ avatar.py
@@ -0,0 +1,50 @@
+# 상하좌우
+
+# n = int(input())
+# x,y = (1,1)
+# plan = input().split()
+
+# dx = [0,0,-1,1]
+# dy = [-1,1,0,0]
+# move = ['L', 'R', 'U', 'D']
+
+# for i in plan:
+#     for j in range(len(move)):
+#         if i == move[j]:
+#             nx = x+dx[j]
+#             ny = y+dy[j]
+#     if nx < 1 or ny < 1 or nx > n or ny > n:
+#         continue
+
+#     x,y=nx,ny
+
+
+# print(x,y)
+
+# 시각
+
+# h = int(input())
+# cnt = 0
+
+# for i in range(h+1):
+#     for j in range(60):
+#         for k in range(60):
+#             if '3' in str(i) + str(j) + str(k):
+#                 cnt+=1
+
+
+# print(cnt, i,j,k)
+
+# 
+
+
+
+
+
+
+
+
+
+
+
+
